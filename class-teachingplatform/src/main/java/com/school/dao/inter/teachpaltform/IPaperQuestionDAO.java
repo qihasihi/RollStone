2e3c83e4378e8e91254d7624ef9c8cb614f13cff
qihaishi <qihaishi@a96fdc72-624f-11df-840f-8e0ec6321a6b>
2014-06-24 08:41:33 +0000

    教学系统修改。

--- class-teachingplatform/src/main/java/com/school/dao/inter/teachpaltform/IPaperQuestionDAO.java
+++
@@ -1,8 +0,0 @@
-
-package com.school.dao.inter.teachpaltform;
-
-import com.school.dao.base.ICommonDAO;
-
-public interface IPaperQuestionDAO extends ICommonDAO<PaperQuestion>{
-
-}
--- class-teachingplatform/src/main/java/com/school/manager/inter/teachpaltform/IPaperQuestionManager.java
+++
@@ -1,8 +0,0 @@
-
-package  com.school.manager.inter.teachpaltform;
-
-import com.school.manager.base.IBaseManager;
-
-public interface IPaperQuestionManager  extends IBaseManager<PaperQuestion> { 
-
-} 
