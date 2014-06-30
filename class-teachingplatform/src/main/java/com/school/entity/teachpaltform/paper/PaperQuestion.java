0615ff422c00bfdd99268a39848d9eb2683e55b8
qihaishi <qihaishi@a96fdc72-624f-11df-840f-8e0ec6321a6b>
2014-06-30 08:11:40 +0000

    教学系统修改。

--- class-teachingplatform/src/main/java/com/school/entity/teachpaltform/paper/PaperQuestion.java
+++ class-teachingplatform/src/main/java/com/school/entity/teachpaltform/paper/PaperQuestion.java
@@ -1,9 +1,11 @@
 package  com.school.entity.teachpaltform.paper;
 
 import com.school.entity.teachpaltform.QuestionInfo;
+import com.school.util.UtilTool;
 
 import javax.persistence.Entity;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Date;
 
 @Entity
@@ -16,84 +18,109 @@ public class PaperQuestion implements Serializable{
     private Date ctime;
     private Float score;
 
-    private QuestionInfo questionInfo;
+    /**
+     * 获取问题的字段
+     */
+    public  Long getQuestionid(){
+        return this.getQuestioninfo().getQuestionid();
+    }
+    public void setQuestionid(Long questionid){
+        this.getQuestioninfo().setQuestionid(questionid);
+    }
 
-    public java.lang.Long getQuestionid(){
-        return this.getQuestionInfo().getQuestionid();
+
+    public java.lang.Integer getQuestiontype(){
+        return this.getQuestioninfo().getQuestiontype();
     }
-    public void setQuestionid(java.lang.Long questionid){
-        this.getQuestionInfo().setQuestionid(questionid);
+    public void setQuestiontype(java.lang.Integer questiontype){
+        this.getQuestioninfo().setQuestiontype(questiontype);
     }
-    //question_type
-    public Integer getQuestiontype(){
-        return this.getQuestionInfo().getQuestiontype();
+    public java.lang.String getContent(){
+        String content=this.getQuestioninfo().getContent();
+        if(content!=null&&content.trim().length()>0){
+            String t= UtilTool.utilproperty.getProperty("RESOURCE_QUESTION_IMG_PARENT_PATH")+"/"+this.getQuestionid()+"/";
+            while(content.indexOf("_QUESTIONPIC+")!=-1)
+                content=content.replace("_QUESTIONPIC+",t);
+            while (content.indexOf("\n")!=-1||content.indexOf("\n\r")!=-1||content.indexOf("\t")!=-1){
+                content=content.replace("\n\r", "<br>&nbsp;&nbsp;");
+                content=content.replace("\n", "<br>");
+                content=content.replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
+                //content=content.replace(" ", "&nbsp;");
+                //s=s.replace("\"", "\\"+"\"");//如果原文含有双引号
+            }
+        }
+        return content;
     }
-    public void setQuestiontype(Integer questiontype){
-        this.getQuestionInfo().setQuestiontype(questiontype);
+    public void setContent(java.lang.String content){
+        this.getQuestioninfo().setContent(content);
     }
-    //paper_type_id
-    public Integer getPapertypeid(){
-        return this.getQuestionInfo().getPapertypeid();
+    public java.lang.String getAnalysis(){
+        return  this.getQuestioninfo().getAnalysis();
     }
-    public void setPapertypeid(Integer paperid){
-        this.getQuestionInfo().setPapertypeid(paperid);
+    public void setAnalysis(java.lang.String analysis){
+        this.getQuestioninfo().setAnalysis(analysis);
     }
-    //question_level
-    public Integer getQuestionlevel(){
-        return this.getQuestionInfo().getQuestionlevel();
-    }
-    public void setQuestionlevel(Integer level){
-        this.getQuestionInfo().setQuestionlevel(level);
-    }
-    //question_level
     public Integer getStatus(){
-        return this.getQuestionInfo().getStatus();
+        return  this.getQuestioninfo().getStatus();
     }
     public void setStatus(Integer status){
-        this.getQuestionInfo().setStatus(status);
+        this.getQuestioninfo().setStatus(status);
     }
-    //cloud_status
-    public Integer getCloudstatus(){
-        return this.getQuestionInfo().getCloudstatus();
+    public java.lang.Integer getCloudstatus(){
+        return this.getQuestioninfo().getCloudstatus();
     }
-    public void setCloudstatus(Integer cstatus){
-        this.getQuestionInfo().setCloudstatus(cstatus);
+    public void setCloudstatus(java.lang.Integer cloudstatus){
+        this.getQuestioninfo().setCloudstatus(cloudstatus);
     }
-    //content
-    public String getContent(){
-        return this.getQuestionInfo().getContent();
+    public java.lang.Integer getQuestionlevel(){
+        return this.getQuestioninfo().getQuestionlevel();
     }
-    public void setContent(String cstatus){
-        this.getQuestionInfo().setContent(cstatus);
+    public void setQuestionlevel(java.lang.Integer questionlevel){
+        this.getQuestioninfo().setQuestionlevel(questionlevel);
     }
-    //analysis
-    public String getAnalysis(){
-        return this.getQuestionInfo().getAnalysis();
+
+
+
+
+    private QuestionInfo questioninfo;
+
+    public QuestionInfo getQuestioninfo() {
+        if(questioninfo==null)
+            questioninfo=new QuestionInfo();
+        return questioninfo;
     }
-    public void setAnalysis(String cstatus){
-        this.getQuestionInfo().setAnalysis(cstatus);
+    public void setQuestioninfo(QuestionInfo questioninfo) {
+        this.questioninfo = questioninfo;
     }
+
+
+
+    //paper_type_id
+    public Integer getPapertypeid(){
+        return this.getQuestioninfo().getPapertypeid();
+    }
+    public void setPapertypeid(Integer paperid){
+        this.getQuestioninfo().setPapertypeid(paperid);
+    }
+
     //use_count
     public Integer getUsecount(){
-        return this.getQuestionInfo().getUsecount();
+        return this.getQuestioninfo().getUsecount();
     }
     public void setUsecount(Integer cstatus){
-        this.getQuestionInfo().setUsecount(cstatus);
+        this.getQuestioninfo().setUsecount(cstatus);
     }
     //correct_answer
     public String getCorrectanswer(){
-        return this.getQuestionInfo().getCorrectanswer();
+        return this.getQuestioninfo().getCorrectanswer();
     }
     public void setCorrectanswer(String cstatus){
-        this.getQuestionInfo().setCorrectanswer(cstatus);
+        this.getQuestioninfo().setCorrectanswer(cstatus);
     }
 
     private PaperInfo paperInfo;
     public void setPaperid(Long paperid){
         this.getPaperInfo().setPaperid(paperid);
-    }
-    public Long getPaperid(){
-        return this.getPaperInfo().getPaperid();
     }
     public String getPapername(){
         return this.getPaperInfo().getPapername();
@@ -114,14 +141,6 @@ public class PaperQuestion implements Serializable{
     public void setPapertype(Integer ptype){
         this.getPaperInfo().setPapertype(ptype);
     }
-    public QuestionInfo getQuestionInfo() {
-        return (questionInfo=(questionInfo==null?new QuestionInfo():questionInfo));
-    }
-
-    public void setQuestionInfo(QuestionInfo questionInfo) {
-        this.questionInfo = questionInfo;
-    }
-
     public PaperInfo getPaperInfo() {
         return (paperInfo=(paperInfo==null?new PaperInfo():paperInfo));
     }
@@ -137,6 +156,7 @@ public class PaperQuestion implements Serializable{
     public void setOrderidx(java.lang.Integer orderidx){
       this.orderidx = orderidx;
     }
+
     public java.lang.Integer getRef(){
       return ref;
     }
