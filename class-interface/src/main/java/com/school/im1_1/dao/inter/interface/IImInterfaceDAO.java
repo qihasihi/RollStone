95ad8e2c99f125263a771446023cca79d50d167c
yuechunyang <yuechunyang@a96fdc72-624f-11df-840f-8e0ec6321a6b>
2014-07-24 08:50:51 +0000

    新建接口模块儿目录

---
+++ class-interface/src/main/java/com/school/im1_1/dao/inter/interface/IImInterfaceDAO.java
@@ -0,0 +1,7 @@
+package com.school.im1_1.dao;
+
+/**
+ * Created by yuechunyang on 14-7-24.
+ */
+public interface IImInterfaceDAO {
+}
---
+++ class-interface/src/main/java/com/school/im1_1/entity/interface/ImInterfaceInfo.java
@@ -0,0 +1,7 @@
+package com.school.im1_1.entity;
+
+/**
+ * Created by yuechunyang on 14-7-24.
+ */
+public class ImInterfaceInfo {
+}
