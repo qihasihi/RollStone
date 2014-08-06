3b2b1eb653b92623d4ccfa8b19dd29b5bc38397a
panfei <panfei@a96fdc72-624f-11df-840f-8e0ec6321a6b>
2014-08-06 03:04:16 +0000

    合并多校版本

--- class-basicplatform/src/main/java/com/school/dao/UserDAO.java
+++ class-basicplatform/src/main/java/com/school/dao/UserDAO.java
@@ -454,7 +454,7 @@ public class UserDAO extends CommonDAO<UserInfo> implements IUserDAO {
 			presult.setRecTotal(Integer.parseInt(objArray[0].toString().trim()));				
 		return userList; 		
 	}
-	
+
 	public List<UserInfo> getUserByCondition(String year,boolean isselstu,boolean isseljz,RoleUser obj,int dc_school_id,
 			PageResult presult) {
 		StringBuilder sqlbuilder=new StringBuilder("{CALL user_condition_proc_search_split(");
@@ -570,16 +570,15 @@ public class UserDAO extends CommonDAO<UserInfo> implements IUserDAO {
 		return null;
 	}
 
-    public List<UserInfo> getUserNotCompleteTask(TpTaskInfo t,String flag) {
+    public List<UserInfo>getUserNotCompleteTask(Long taskid,Integer userid,Integer classid,String flag){
         StringBuilder sqlbuilder = new StringBuilder("{call tp_task_notcomplete_proc_split(");
         List<Object> objList = new ArrayList<Object>();
-        if(t==null||t.getTaskid()==null||t.getCourseid()==null)
+        if(taskid==null)
             return null;
-        sqlbuilder.append("?,?,");
-        objList.add(t.getTaskid());
-        objList.add(t.getCourseid());
-        if(t.getUserid()!=null){
-            objList.add(t.getUserid());
+        sqlbuilder.append("?,NULL,");
+        objList.add(taskid);
+        if(userid!=null){
+            objList.add(userid);
             sqlbuilder.append("?,");
         }else
             sqlbuilder.append("null,");
@@ -588,6 +587,12 @@ public class UserDAO extends CommonDAO<UserInfo> implements IUserDAO {
             sqlbuilder.append("?,");
         }else
             sqlbuilder.append("null,");
+        if(classid!=null){
+            objList.add(classid);
+            sqlbuilder.append("?,");
+        }else
+            sqlbuilder.append("null,");
+
         sqlbuilder.append("?)}");
         List<Integer> types=new ArrayList<Integer>();
         types.add(Types.INTEGER);
