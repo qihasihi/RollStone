33455c4fb73272298789db4dd0300add47768dcb
yuchf <yuchf@a96fdc72-624f-11df-840f-8e0ec6321a6b>
2014-10-16 02:06:22 +0000

    已对CutProc类进行修改

--- class-common/src/main/java/com/school/util/CutProc.java
+++ class-common/src/main/java/com/school/util/CutProc.java
@@ -8,7 +8,7 @@ public class CutProc {
     private PrintWriter writer = null;
     public CutProc(){}
 
-    public String cutProc(String path) {                //参数path为文件的绝对路径
+    public boolean cutProc(String path) {                //参数path为文件的绝对路径
         File file = new File(path);
         path = file.getParent();                        //获取文件所在目录
         try {
@@ -23,42 +23,44 @@ public class CutProc {
         while(true) {                                   //外循环读取全部文件，直到读到文件结尾捕获异常退出
             try {
                 String line = reader.readLine();
-                if(line.contains("DELIMITER $$")) {   //判断存储过程开头
+                do {
                     line = reader.readLine();
-                    while(!line.contains("DEFINER")) {
-                        line = reader.readLine();
-                    }
-                    String []args = line.split("`");
-                    File f = new File(path + "/"+args[5]+".sql");
-                    out = new FileOutputStream(f);
-                    writer = new PrintWriter(out);
-                    writer.println("DELIMITER $$");
-                    writer.println("");
-                    writer.println("USE `m_school`$$");
-                    writer.println("");
-                    writer.println("DROP PROCEDURE IF EXISTS `" + args[5] + "`$$");
-                    writer.println("");
-                    line = line.substring(9);                   //截取首句前面的“/*!50003 ”
-                    while(!line.contains("*/$$")) {
-                        try {
-                            writer.println(line);
-                            line = reader.readLine();
-                        } catch (Exception e) {
-                            System.out.println(e.getStackTrace());
-                        }
-                    }
+                    if (line == null)
+                        return true;
+                    System.out.println("in while:" + line);
+                } while(!line.contains("DEFINER"));
+                String []args = line.split("`");
+                File f = new File(path + "/"+args[5]+".sql");
+                out = new FileOutputStream(f);
+                writer = new PrintWriter(out);
+                writer.println("DELIMITER $$");
+                writer.println("");
+                writer.println("USE `m_school`$$");
+                writer.println("");
+                writer.println("DROP PROCEDURE IF EXISTS `" + args[5] + "`$$");
+                writer.println("");
+                line = line.substring(9);                   //截取首句前面的“/*!50003 ”
+                while(!line.contains("*/$$")) {
                     try {
-                        writer.println("END $$");
-                        writer.println("");
-                        writer.println("DELIMITER ;");
-                        writer.close();
-                        out.close();
-                    } catch(Exception e) {
+                        writer.println(line);
+                        line = reader.readLine();
+                    } catch (Exception e) {
                         System.out.println(e.getStackTrace());
+                        return false;
                     }
-                }                   //End with "DELITER ;"
+                }
+                try {
+                    writer.println("END $$");
+                    writer.println("");
+                    writer.println("DELIMITER ;");
+                    writer.close();
+                    out.close();
+                } catch(Exception e) {
+                    System.out.println(e.getStackTrace());
+                    return false;
+                }
             } catch(Exception e) {                          //读到文件结尾捕获异常
-                return "已完成分割";
+                return false;
             }
         }
     }
