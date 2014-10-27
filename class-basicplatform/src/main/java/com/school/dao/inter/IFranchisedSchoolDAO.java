7619cb7d443c0c0c4aae9250cf9327b37474865a
yuchf <yuchf@a96fdc72-624f-11df-840f-8e0ec6321a6b>
2014-10-27 07:03:37 +0000

    删除无效类

--- class-basicplatform/src/main/java/com/school/dao/inter/IFranchisedSchoolDAO.java
+++
@@ -1,5 +0,0 @@
-package com.school.dao.inter;
-
-public interface IFranchisedSchoolDAO {
-    public int getTotalClass(int schoolId, String classYearId);
-}
--- class-basicplatform/src/main/java/com/school/manager/inter/IFranchisedSchoolManager.java
+++
@@ -1,5 +0,0 @@
-package com.school.manager.inter;
-
-public interface IFranchisedSchoolManager {
-    public int getTotalClass(int schoolId, String classYear);
-}
