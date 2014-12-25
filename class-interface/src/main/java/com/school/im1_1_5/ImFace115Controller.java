591d8be1c9c7ea0623c57fb5de23198b391aeb50
yuechunyang <yuechunyang@a96fdc72-624f-11df-840f-8e0ec6321a6b>
2014-12-25 05:42:27 +0000

    修改学生课程日历分校id的数据来源

--- class-interface/src/main/java/com/school/im1_1_5/ImFace115Controller.java
+++ class-interface/src/main/java/com/school/im1_1_5/ImFace115Controller.java
@@ -123,12 +123,13 @@ public class ImFace115Controller extends BaseController {
             response.getWriter().print("{\"result\":\"0\",\"msg\":\"当前用户未绑定，请联系管理员\"}");
             return;
         }
+        Integer schoolId=this.getDcSchoolIdByClassId(Integer.parseInt(classid));
         ImInterfaceInfo obj = new ImInterfaceInfo();
-        obj.setSchoolid(Integer.parseInt(schoolid));
+        obj.setSchoolid(schoolId);
         obj.setClassid(Integer.parseInt(classid));
         Map m = new HashMap();
         Map m2 = new HashMap();
-        List<Map<String,Object>> courseList = this.imInterfaceManager.getStudentCalendar(userList.get(0).getUserid(),Integer.parseInt(schoolid),Integer.parseInt(classid),Integer.parseInt(year),Integer.parseInt(month));
+        List<Map<String,Object>> courseList = this.imInterfaceManager.getStudentCalendar(userList.get(0).getUserid(),schoolId,Integer.parseInt(classid),Integer.parseInt(year),Integer.parseInt(month));
         if(utype!=2){
             long mTime = System.currentTimeMillis();
             int offset = Calendar.getInstance().getTimeZone().getRawOffset();
@@ -138,7 +139,7 @@ public class ImFace115Controller extends BaseController {
             String currentMonth = currentDay.split("-")[1];
             String currentYear = currentDay.split("-")[0];
             if(Integer.parseInt(month)==Integer.parseInt(currentMonth)&&Integer.parseInt(year)==Integer.parseInt(currentYear)){
-                List<Map<String,Object>> courseArray = this.imInterfaceManager.getstudentCalendarDetail(userList.get(0).getUserid(), utype, Integer.parseInt(classid), Integer.parseInt(schoolid), currentDay);
+                List<Map<String,Object>> courseArray = this.imInterfaceManager.getstudentCalendarDetail(userList.get(0).getUserid(), utype, Integer.parseInt(classid), schoolId, currentDay);
                 if(courseArray!=null&&courseArray.size()>0){
                     List<Map<String,Object>> courseArray2 = new ArrayList<Map<String, Object>>();
                     Date d = new Date();
@@ -260,217 +261,6 @@ public class ImFace115Controller extends BaseController {
     }
 
     /**
-     * 教师班级课表接口
-     * @param request
-     * @param mp
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(params="m=TeaClassCalendar",method= {RequestMethod.GET,RequestMethod.POST})
-    public void getTeaClassCalendar(HttpServletRequest request,HttpServletResponse response,ModelMap mp)throws Exception{
-        JsonEntity je = new JsonEntity();
-        String userid = request.getParameter("jid");
-        String usertype=request.getParameter("userType");
-        String schoolid = request.getParameter("schoolId");
-        String month = request.getParameter("requestMonth");
-        String year = request.getParameter("requestYear");
-        String timestamp = request.getParameter("time");
-        String sig = request.getParameter("sign");
-        if(!ImFace115Util.ValidateRequestParam(request)){
-            JSONObject jo=new JSONObject();
-            jo.put("result","0");
-            jo.put("msg",UtilTool.msgproperty.getProperty("PARAM_ERROR").toString());
-            jo.put("data","");
-            response.getWriter().print(jo.toString());
-            return;
-        }
-        HashMap<String,String> map = new HashMap();
-        map.put("jid",userid);
-        map.put("userType",usertype);
-        map.put("schoolId",schoolid);
-        map.put("time",timestamp);
-        map.put("requestMonth",month);
-        map.put("requestYear",year);
-        // String sign = UrlSigUtil.makeSigSimple("StuClassCalendar",map,"*ETT#HONER#2014*");
-        Boolean b = UrlSigUtil.verifySigSimple("TeaClassCalendar",map,sig);
-        if(!b){
-            response.getWriter().print("{\"result\":\"0\",\"message\":\"验证失败，非法登录\"}");
-            return;
-        }
-        int utype = ImFace115Util.getUserType(usertype);
-        UserInfo ui = new UserInfo();
-        ui.setEttuserid(Integer.parseInt(userid));
-        List<UserInfo> userList = this.userManager.getList(ui,null);
-        if(userList==null||userList.size()<1){
-            response.getWriter().print("{\"result\":\"0\",\"msg\":\"当前用户未绑定，请联系管理员\"}");
-            return;
-        }
-        List<Map<String,Object>> courseList = this.imInterfaceManager.getTeacherCalendar(userList.get(0).getUserid(), Integer.parseInt(schoolid), Integer.parseInt(year), Integer.parseInt(month));
-        Map m = new HashMap();
-        Map m2 = new HashMap();
-        long mTime = System.currentTimeMillis();
-        int offset = Calendar.getInstance().getTimeZone().getRawOffset();
-        Calendar c = Calendar.getInstance();
-        c.setTime(new Date(mTime - offset));
-        String currentDay =UtilTool.DateConvertToString(c.getTime(),UtilTool.DateType.type1);
-        String currentMonth = currentDay.split("-")[1];
-        if(Integer.parseInt(month)==Integer.parseInt(currentMonth)){
-            List<Map<String,Object>> courseArray = this.imInterfaceManager.getTeacherCalendarDetail(userList.get(0).getUserid(), utype, Integer.parseInt(schoolid), currentDay);
-            if(courseArray!=null&&courseArray.size()>0){
-                List<Map<String,Object>> courseArray2 = new ArrayList<Map<String, Object>>();
-                Date d = new Date();
-                for(int i = 0;i<courseArray.size();i++){
-                    Map o = courseArray.get(i);
-                    Map o2 = new HashMap();
-                    o2.put("courseId",o.get("COURSE_ID"));
-                    o2.put("courseName",o.get("COURSE_NAME"));
-                    if(o.get("BEGIN_TIME")!=null&&o.get("END_TIME")!=null){
-                        int thisDay = d.getDay();
-                        Date btime = UtilTool.StringConvertToDate(o.get("BEGIN_TIME").toString(),UtilTool.DateType.type1);
-                        int bDay = btime.getDay();
-                        Date etime = UtilTool.StringConvertToDate(o.get("END_TIME").toString(),UtilTool.DateType.type1);
-                        int eDay = etime.getDay();
-                        if(thisDay==bDay&&thisDay==eDay){
-                            if(o.get("BEGIN_TIME").toString().indexOf(":")>0&&o.get("END_TIME").toString().indexOf(":")>0){
-                                String coureBtime = o.get("BEGIN_TIME").toString().substring((o.get("BEGIN_TIME").toString().indexOf(" ")+1),o.get("BEGIN_TIME").toString().lastIndexOf(":"));
-                                String coureEtime = o.get("END_TIME").toString().substring((o.get("END_TIME").toString().indexOf(" ")+1),o.get("END_TIME").toString().lastIndexOf(":"));
-                                o2.put("courseDate",coureBtime+"~"+coureEtime);
-                            }else{
-                                o2.put("courseDate","");
-                            }
-                        }else{
-                            o2.put("courseDate","");
-                        }
-                    }else{
-                        o2.put("courseDate","");
-                    }
-                    //o2.put("courseDate",o.get("BEGIN_TIME")+"~"+o.get("END_TIME")!=null?o.get("END_TIME"):"——");
-                    o2.put("schoolId",o.get("DC_SCHOOL_ID"));
-                    o2.put("classId",o.get("CLASS_ID"));
-                    o2.put("classType",o.get("CLASS_TYPE"));
-                    o2.put("className",o.get("CLASSNAME"));
-                    courseArray2.add(o2);
-                }
-                m2.put("courseArray",courseArray2);
-            }else{
-                m2.put("courseArray",null);
-            }
-        }
-        if(courseList!=null&&courseList.size()>0){
-            List<Map<String,Object>> courseList2 = new ArrayList<Map<String, Object>>();
-            for(int i = 0 ;i<courseList.size();i++){
-                Map o = courseList.get(i);
-                Map o2 = new HashMap();
-                o2.put("orderDay",o.get("E_DAY"));
-                o2.put("hasCourse",o.get("HASCOURSE"));
-                courseList2.add(o2);
-            }
-            m2.put("courseList",courseList2);
-        }else{
-            m.put("result","0");
-            m.put("msg","当前没有专题");
-        }
-        m.put("result","1");
-        m.put("msg","成功");
-        m.put("data",m2);
-        JSONObject object = JSONObject.fromObject(m);
-        response.getWriter().print(object.toString());
-    }
-
-    /**
-     * 教师班级课表接口详细
-     * @param request
-     * @param mp
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(params="m=TeaClassCalendarDetail",method= {RequestMethod.GET,RequestMethod.POST})
-    public void getTeaClassCalendarByDay(HttpServletRequest request,HttpServletResponse response,ModelMap mp)throws Exception{
-        JsonEntity je = new JsonEntity();
-        String userid = request.getParameter("jid");
-        String usertype=request.getParameter("userType");
-        String schoolid = request.getParameter("schoolId");
-        String currentDay = request.getParameter("requestDay");
-        String timestamp = request.getParameter("time");
-        String sig = request.getParameter("sign");
-        if(!ImFace115Util.ValidateRequestParam(request)){
-            JSONObject jo=new JSONObject();
-            jo.put("result","0");
-            jo.put("msg",UtilTool.msgproperty.getProperty("PARAM_ERROR").toString());
-            jo.put("data","");
-            response.getWriter().print(jo.toString());
-            return;
-        }
-        HashMap<String,String> map = new HashMap();
-        map.put("jid",userid);
-        map.put("userType",usertype);
-        map.put("schoolId",schoolid);
-        map.put("time",timestamp);
-        map.put("requestDay",currentDay);
-        // String sign = UrlSigUtil.makeSigSimple("StuClassCalendar",map,"*ETT#HONER#2014*");
-        Boolean b = UrlSigUtil.verifySigSimple("TeaClassCalendarDetail",map,sig);
-        if(!b){
-            response.getWriter().print("{\"result\":\"0\",\"message\":\"验证失败，非法登录\"}");
-            return;
-        }
-        int utype = ImFace115Util.getUserType(usertype);
-        UserInfo ui = new UserInfo();
-        ui.setEttuserid(Integer.parseInt(userid));
-        List<UserInfo> userList = this.userManager.getList(ui,null);
-        if(userList==null||userList.size()<1){
-            response.getWriter().print("{\"result\":\"0\",\"msg\":\"当前用户未绑定，请联系管理员\"}");
-            return;
-        }
-        List<Map<String,Object>> courseArray = this.imInterfaceManager.getTeacherCalendarDetail(userList.get(0).getUserid(),utype, Integer.parseInt(schoolid), currentDay);
-        Map m = new HashMap();
-        Map m2 = new HashMap();
-        if(courseArray!=null&&courseArray.size()>0){
-            List<Map<String,Object>> courseArray2 = new ArrayList<Map<String, Object>>();
-            Date d = new Date();
-            for(int i = 0;i<courseArray.size();i++){
-                Map o = courseArray.get(i);
-                Map o2 = new HashMap();
-                o2.put("courseId",o.get("COURSE_ID"));
-                o2.put("courseName",o.get("COURSE_NAME"));
-                if(o.get("BEGIN_TIME")!=null&&o.get("END_TIME")!=null){
-                    int thisDay = d.getDay();
-                    Date btime = UtilTool.StringConvertToDate(o.get("BEGIN_TIME").toString(),UtilTool.DateType.type1);
-                    int bDay = btime.getDay();
-                    Date etime = UtilTool.StringConvertToDate(o.get("END_TIME").toString(),UtilTool.DateType.type1);
-                    int eDay = etime.getDay();
-                    if(thisDay==bDay&&thisDay==eDay){
-                        if(o.get("BEGIN_TIME").toString().indexOf(":")>0&&o.get("END_TIME").toString().indexOf(":")>0){
-                            String coureBtime = o.get("BEGIN_TIME").toString().substring((o.get("BEGIN_TIME").toString().indexOf(" ")+1),o.get("BEGIN_TIME").toString().lastIndexOf(":"));
-                            String coureEtime = o.get("END_TIME").toString().substring((o.get("END_TIME").toString().indexOf(" ")+1),o.get("END_TIME").toString().lastIndexOf(":"));
-                            o2.put("courseDate",coureBtime+"~"+coureEtime);
-                        }else{
-                            o2.put("courseDate","");
-                        }
-                    }else{
-                        o2.put("courseDate","");
-                    }
-                }else{
-                    o2.put("courseDate","");
-                }
-                //o2.put("courseDate",o.get("BEGIN_TIME")+"~"+o.get("END_TIME")!=null?o.get("END_TIME"):"——");
-                o2.put("schoolId",o.get("DC_SCHOOL_ID"));
-                o2.put("classId",o.get("CLASS_ID"));
-                o2.put("classType",o.get("CLASS_TYPE"));
-                o2.put("className",o.get("CLASSNAME"));
-                courseArray2.add(o2);
-            }
-            m2.put("courseArray",courseArray2);
-        }else{
-            m2.put("courseArray",null);
-        }
-        m.put("result","1");
-        m.put("msg","成功");
-        m.put("data",m2);
-        JSONObject object = JSONObject.fromObject(m);
-        response.getWriter().print(object.toString());
-    }
-
-    /**
      * 学生班级课表接口详细
      * @param request
      * @param mp
@@ -516,7 +306,8 @@ public class ImFace115Controller extends BaseController {
             response.getWriter().print("{\"result\":\"0\",\"msg\":\"当前用户未绑定，请联系管理员\"}");
             return;
         }
-        List<Map<String,Object>> courseArray = this.imInterfaceManager.getstudentCalendarDetail(userList.get(0).getUserid(), utype,Integer.parseInt(classid), Integer.parseInt(schoolid), currentDay);
+        Integer schoolId=this.getDcSchoolIdByClassId(Integer.parseInt(classid));
+        List<Map<String,Object>> courseArray = this.imInterfaceManager.getstudentCalendarDetail(userList.get(0).getUserid(), utype,Integer.parseInt(classid), schoolId, currentDay);
         Map m = new HashMap();
         Map m2 = new HashMap();
         if(courseArray!=null&&courseArray.size()>0){
